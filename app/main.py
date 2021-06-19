062b57a804e9d79e2a3d28b7c1b7cba397937ece
sergio2526 <sergiorubiano25@hotmail.com>
2021-06-19 18:29:58 -0500

    agregando archivo

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
+from grevillea import Grevillea
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root(request):
     return {"Hello": "World"}
+
 
 @app.post("/items/{item}")
 def read_item(item : int):
     return {"item_id": item}
+
+handler = Grevillea(app) 
